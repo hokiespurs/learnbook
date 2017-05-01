74f8b04cffe6f9508feebc4cda577cf655bc8faf
Richie Slocum <richie.slocum@cormorantanalytics.com>
2017-05-01 01:00:29 -0700

    clean up lsr

--- chapters/leastsquares/alg/sandbox/lsr.m
+++ chapters/leastsquares/alg/sandbox/lsr.m
@@ -47,7 +47,7 @@ expectedType = {'ols','wls','gls','lin','linear',...
 isfunction = @(x)(isa(x,'function_handle'));
 
 addRequired(p,'x',@isnumeric);
-addRequired(p,'y',@isnumeric);
+addRequired(p,'y',@(x)(isnumeric(x) & size(x,2)==1));
 addRequired(p,'modelfun',isfunction);
 
 addOptional(p,'betacoef0',defaultBeta0Cov,@isnumeric);
@@ -63,195 +63,43 @@ addParameter(p,'maxiter',defaultMaxIter,@(x)(isscalar(x) & isnumeric(x)));
 addParameter(p,'betacoef0asObs',defaultBetacoefAsObs,@islogical);
 addParameter(p,'chi2alpha',defaultchi2,@(x)(isscalar(x) & isnumeric(x)));
 parse(p,x,y,modelfun,varargin{:});
-%% Check Input Logic
-%% Determine Level of output to screen
+ErrorModelInfo.parser = p; % include function input in metadata output
+
+%% ADDITIONAL INPUT CHECKS
+% Determine Level of output to screen
 isverbose = p.Results.verbose;
-hline = repmat('-',1,50);
-%% check what type of least squares
-if ismember(p.Results.type,expectedType(1:5))
-    ErrorModelInfo.meta.type = 'linear';
-    if isverbose
-       fprintf('\n%s\nPerforming Linear Least Squares\n%s\n',hline,hline); 
-    end
-    islinear = true;
-    isnonlinear = false;
-    istls = false;
-elseif ismember(p.Results.type,expectedType(6:7))
-    ErrorModelInfo.meta.type = 'nonlinear';
-    if isverbose
-       fprintf('\n%s\nPerforming NonLinear Least Squares\n%s\n',hline,hline); 
-    end
-    islinear = false;
-    isnonlinear = true;
-    istls = false;
-else
-    ErrorModelInfo.meta.type = 'total';
-    if isverbose
-       fprintf('\n%s\nPerforming Total Least Squares\n%s\n',hline,hline); 
-    end
-    islinear = false;
-    isnonlinear = false;
-    istls = true;
-end
-%% Make sure y matrix is ok
-if size(y,2)~=1
-   error('Response vector y must be a (mx1) vector'); 
-end
-%% determine nObsEqns, nObservations, nPredictors, nEqnPerObservations
+
+% check what type of least squares
+[ErrorModelInfo.meta.type, islinear, isnonlinear, istls] = getLsrType(p,expectedType,isverbose);
+
+% determine nObsEqns, nObservations, nPredictors, nEqnPerObservations
 nObsEqns = numel(y);
-nObservations = size(x,1);
 nPredictors = numel(x);
-nEqnPerObservations = nObsEqns/nObservations;
-if islinear %no explicit info about betacoef is input for linear functions 
-   for iTestBeta=1:100 %loop and test different nBetacoef and see what works
-       try
-           modelfun(zeros(iTestBeta,1),x);
-           nBetacoef = iTestBeta;
-           break;
-       catch
-           % keep looping until is works
-           % maybe I shouldnt have tried to force linear into this function
-       end
-   end
-else
-   nBetacoef = numel(p.Results.betacoef0);
-end
+nBetacoef = calcNbetacoef(modelfun,x);
 dof = nObsEqns-nBetacoef; %degrees of freedom
 if dof<=0
    error('Must be an overconstrained system of equations'); 
 end
-%% assemble stochastic model into covariance matrix covY (or covX for TLS)
-if isempty(p.Results.stochastic) %default to identity matrix
-    ErrorModelInfo.meta.stochastic = 'N/A';
-    if isverbose
-        fprintf('Stochastic Model : N/A\n');
+% assemble stochastic model into covariance matrix covY (or covX for TLS)
+[ErrorModelInfo.meta.stochastic,covX,covY] = calcStochastic(p,isverbose,nPredictors,nObsEqns,istls);
+
+% do checks specific to nonlinear and tls functions
+if (isnonlinear || istls)
+    if isempty(p.Results.betacoef0) %ensure initial beta guess is  for nonlinear and tls
+        error('Need an initial guess at Beta coefficients for Nonlinear or TLS');
     end
-    if istls
-        covX = eye(nPredictors);
-    else
-        covY = eye(nObsEqns);
-    end
-elseif isvector(p.Results.stochastic) %weight for each observation equation
-    ErrorModelInfo.meta.stochastic = 'Weight Vector';
-    if isverbose
-       fprintf('Stochastic Model : Weight Vector\n'); 
-    end
-    if istls
-        error('stochastic input must be a covariance matrix when doing tls, cant be a vector');
-    end
-    if numel(p.Results.stochastic)~=nObsEqns
-        error('stochastic (weight) vector must be the same number of elements as y')
-    end
-    covY = inv(diag(p.Results.stochastic));
-else % user input is covariance
-    ErrorModelInfo.meta.stochastic = 'Covariance';
-    if isverbose
-       fprintf('Stochastic Model : Covariance\n'); 
-    end
-    if istls
-        covX = p.Results.stochastic;
-        if size(covX,1)~=nPredictors || ~issymmetric(covX)
-            error('Invalid Covariance matrix, must be symmetric with size [%.0f x %.0f]',...
-                nPredictors,nPredictors);
-        end
-    else
-        covY = p.Results.stochastic;
-        if size(covY,1)~=nObsEqns || ~issymmetric(covY)
-            error('Invalid Covariance matrix, must be symmetric with size [%.0f x %.0f]',...
-                nPredictors,nPredictors);
-        end
-    end
-end
-%% ensure initial beta guess is input
-if (isnonlinear || istls) && isempty(p.Results.betacoef0)
-    error('Need an initial guess at Beta coeffiicents for Nonlinear or TLS');
-end
-if isnonlinear || istls
    betacoef0 = p.Results.betacoef0(:); %ensure one column
+   [ErrorModelInfo.meta.Jtype,Jfun]=checkJ(p,betacoef0,x,y,isverbose);
+   if istls
+       [ErrorModelInfo.meta.Btype,Bfun]=checkB(p,isverbose,betacoef0,x,y);
+   end
 end
-%% test Jacobian
-if isnonlinear || istls
-    if ismember('analyticalJ',p.UsingDefaults)
-        ErrorModelInfo.meta.Jtype = 'numerical';
-        if isverbose
-            fprintf('Jacobian Function: Numerical\n');
-        end
-    else
-        ErrorModelInfo.meta.Jtype = 'analytical';
-        if isverbose
-            fprintf('Jacobian Function: Analytical\n');
-        end
-    end
-    Jfun = p.Results.analyticalJ;
-    % make sure the function works
-    try
-       foo=Jfun(betacoef0,x)*betacoef0;
-       if sum(size(foo)~=size(y))>0
-           error('J*beta should return the same size as the response(y)');
-       end
-    catch Jerr
-        if ismember('analyticalJ',p.UsingDefaults) %default value
-            fprintf('Calculating Jacobian of Modelfun throws an error\n');
-        else
-            fprintf('J Function throws an error\n');
-        end
-        rethrow(Jerr);
-    end
-end
-%% test B Matrix (only TLS)
-if istls
-    if ismember('analyticalB',p.UsingDefaults)
-        ErrorModelInfo.meta.Btype = 'numerical';
-        if isverbose
-            fprintf('B Function       : Numerical\n');
-        end
-    else
-        ErrorModelInfo.meta.Btype = 'analytical';
-        if isverbose
-            fprintf('B Function       : Analytical\n');
-        end
-    end
-    Bfun = p.Results.analyticalB;
-    % make sure the function works
-    try
-       foo=Bfun(betacoef0,x)*x(:);
-       if sum(size(foo)~=size(y))>0
-           error('B function*x(:) should return the same size as the response(y)');
-       end
-    catch Berr
-        if ismember('analyticalB',p.UsingDefaults) %default value
-            fprintf('Calculating B function of Modelfun throws an error\n');
-        else
-            fprintf('B Function throws an error\n');
-        end
-        rethrow(Berr);
-    end
-end
-%% test Beta Covariance
+
+% check and populate Beta Covariance
 betacoef0asobs = p.Results.betacoef0asObs;
 betacoefcov = p.Results.beta0covariance;
-if ~isempty(betacoefcov) 
-    betacoef0asobs = true;
-    if isempty(p.Results.betacoef0) % covariance but no initial guess
-        error('Need to input beta coefficients to add as observation equations with the input covariance');
-    end
-    if ~issymmetric(betacoefcov) || size(betacoefcov,1)~=nBetacoef
-        error('beta0covariance must be a valid covariance matrix with size [%.0f x %.0f)',...
-            nBetacoef,nBetacoef);
-    end
-else
-    betacoefcov = eye(nBetacoef);
-end
-%% Checks for betacoef0 as observations
-if betacoef0asobs && isempty(betacoefcov) && ~isempty(p.Results.stochastic)
-   error('When stochastic input, betacoefasObs = true, need betacoef0covariance');
-elseif betacoef0asobs && ~isempty(betacoefcov) && isempty(p.Results.stochastic)
-   error('When stochastic not input, betacoefasObs = true, cant have betacoef0covariance because itd be weighting everything else as 1, and the beta0covariacne differently');
-elseif islinear && isempty(p.Results.betacoef0) && betacoef0asobs
-    error('Need to input betacoef0 in order to use it as an observation');
-elseif islinear && ~betacoef0asobs && ~isempty(p.Results.betacoef0)
-    warning('betacoef0 isnt doing anything for this linear least squares solution, use betacoef0asobs to include it');
-end
+[betacoef0asobs,betacoefcov] = checkBetacoefCov(p,betacoef0asobs,betacoefcov,nBetacoef,islinear);
+
 %% DO LEAST SQUARES
 if islinear
 %% Calculate Linear Least Squares
@@ -305,7 +153,7 @@ R = V;
 MSE = So2;
 CovB = ErrorModelInfo.covB;
 J = A;
-else
+elseif isnonlinear || istls
 %% Calculate NonLinear Least Squares and TLS
     maxiter = p.Results.maxiter;
     betacoef = betacoef0;                  % set first guess at unknowns
@@ -390,6 +238,9 @@ else
     R = V;
     MSE = So2;
     CovB = ErrorModelInfo.covB;
+else %robust
+%% Robust Least Squares    
+
 end
 %% Do Chi2 Test
 if strcmp(ErrorModelInfo.meta.stochastic, 'Covariance')
@@ -432,9 +283,25 @@ else
         fprintf('Stochastic model isnt covariances, so chi2 test isnt valid\n');
     end    
 end
-%% include function input in metadata output
 
 end
+
+function nBetacoef = calcNbetacoef(modelfun,x)
+   for iTestBeta=1:100 %loop and test different nBetacoef and see what works
+       try
+           modelfun(zeros(iTestBeta,1),x(2,:));
+           nBetacoef = iTestBeta;
+           break;
+       catch
+           % keep looping until modelfun doesnt throw an error
+           % maybe I shouldnt have tried to force linear into this function
+       end
+   end
+   if nBetacoef ==100
+      error('Unable to determine the number of beta coeficients'); 
+   end
+end
+
 function [J,K,W,B]=addBetacoef0TLS(J,K,B,covX,betacoef0,betacoefcov,betacoef)
 
 Jadd = eye(numel(betacoef0));
@@ -488,3 +355,163 @@ K = y - modelfun(betacoef,x);
 
 end
 
+function [stochastictype,covX,covY] = calcStochastic(p,isverbose,nPredictors,nObsEqns,istls)
+%one of these won't get called, and doesnt get used later on
+covX = nan;
+covY = nan;
+
+if isempty(p.Results.stochastic) %default to identity matrix
+    stochastictype = 'N/A';
+    if isverbose
+        fprintf('Stochastic Model : N/A\n');
+    end
+    if istls
+        covX = eye(nPredictors);
+    else
+        covY = eye(nObsEqns);
+    end
+elseif isvector(p.Results.stochastic) %weight for each observation equation
+    stochastictype = 'Weight Vector';
+    if isverbose
+       fprintf('Stochastic Model : Weight Vector\n'); 
+    end
+    if istls
+        error('stochastic input must be a covariance matrix when doing tls, cant be a vector');
+    end
+    if numel(p.Results.stochastic)~=nObsEqns
+        error('stochastic (weight) vector must be the same number of elements as y')
+    end
+    covY = inv(diag(p.Results.stochastic));
+else % user input is covariance
+    stochastictype = 'Covariance';
+    if isverbose
+       fprintf('Stochastic Model : Covariance\n'); 
+    end
+    if istls
+        covX = p.Results.stochastic;
+        if size(covX,1)~=nPredictors || ~issymmetric(covX)
+            error('Invalid Covariance matrix, must be symmetric with size [%.0f x %.0f]',...
+                nPredictors,nPredictors);
+        end
+    else
+        covY = p.Results.stochastic;
+        if size(covY,1)~=nObsEqns || ~issymmetric(covY)
+            error('Invalid Covariance matrix, must be symmetric with size [%.0f x %.0f]',...
+                nPredictors,nPredictors);
+        end
+    end
+end
+
+end
+
+function [Jtype,Jfun]=checkJ(p,betacoef0,x,y,isverbose)
+    if ismember('analyticalJ',p.UsingDefaults)
+        Jtype = 'numerical';
+        if isverbose
+            fprintf('Jacobian Function: Numerical\n');
+        end
+    else
+        Jtype = 'analytical';
+        if isverbose
+            fprintf('Jacobian Function: Analytical\n');
+        end
+    end
+    Jfun = p.Results.analyticalJ;
+    % make sure the function works
+    try
+       foo=Jfun(betacoef0,x)*betacoef0;
+       if any(size(foo)~=size(y))
+           error('J*beta should return the same size as the response(y)');
+       end
+    catch Jerr
+        if ismember('analyticalJ',p.UsingDefaults) %default value
+            fprintf('Calculating Jacobian of Modelfun throws an error\n');
+        else
+            fprintf('J Function throws an error\n');
+        end
+        rethrow(Jerr);
+    end
+end
+
+function [lsrtype, islinear, isnonlinear, istls]=getLsrType(p,expectedType,isverbose)
+hline = repmat('-',1,50);
+if ismember(p.Results.type,expectedType(1:5))
+    lsrtype = 'linear';
+    if isverbose
+       fprintf('\n%s\nPerforming Linear Least Squares\n%s\n',hline,hline); 
+    end
+    islinear = true;
+    isnonlinear = false;
+    istls = false;
+elseif ismember(p.Results.type,expectedType(6:7))
+    lsrtype = 'nonlinear';
+    if isverbose
+       fprintf('\n%s\nPerforming NonLinear Least Squares\n%s\n',hline,hline); 
+    end
+    islinear = false;
+    isnonlinear = true;
+    istls = false;
+else
+    lsrtype = 'total';
+    if isverbose
+       fprintf('\n%s\nPerforming Total Least Squares\n%s\n',hline,hline); 
+    end
+    islinear = false;
+    isnonlinear = false;
+    istls = true;
+end
+end
+
+function [Btype,Bfun]=checkB(p,isverbose,betacoef0,x,y)
+if ismember('analyticalB',p.UsingDefaults)
+    Btype = 'numerical';
+    if isverbose
+        fprintf('B Function       : Numerical\n');
+    end
+else
+    Btype = 'analytical';
+    if isverbose
+        fprintf('B Function       : Analytical\n');
+    end
+end
+Bfun = p.Results.analyticalB;
+% make sure the function works
+try
+    foo=Bfun(betacoef0,x)*x(:);
+    if sum(size(foo)~=size(y))>0
+        error('B function*x(:) should return the same size as the response(y)');
+    end
+catch Berr
+    if ismember('analyticalB',p.UsingDefaults) %default value
+        fprintf('Calculating B function of Modelfun throws an error\n');
+    else
+        fprintf('B Function throws an error\n');
+    end
+    rethrow(Berr);
+end
+end
+
+function [betacoef0asobs,betacoefcov] = checkBetacoefCov(p,betacoef0asobs,betacoefcov,nBetacoef,islinear)
+if ~isempty(betacoefcov)
+    betacoef0asobs = true;
+    if isempty(p.Results.betacoef0) % covariance but no initial guess
+        error('Need to input beta coefficients to add as observation equations with the input covariance');
+    end
+    if ~issymmetric(betacoefcov) || size(betacoefcov,1)~=nBetacoef
+        error('beta0covariance must be a valid covariance matrix with size [%.0f x %.0f)',...
+            nBetacoef,nBetacoef);
+    end
+else
+    betacoefcov = eye(nBetacoef);
+end
+% Checks for betacoef0 as observations
+if betacoef0asobs && isempty(betacoefcov) && ~isempty(p.Results.stochastic)
+   error('When stochastic input, betacoefasObs = true, need betacoef0covariance');
+elseif betacoef0asobs && ~isempty(betacoefcov) && isempty(p.Results.stochastic)
+   error('When stochastic not input, betacoefasObs = true, cant have betacoef0covariance because itd be weighting everything else as 1, and the beta0covariacne differently');
+elseif islinear && isempty(p.Results.betacoef0) && betacoef0asobs
+    error('Need to input betacoef0 in order to use it as an observation');
+elseif islinear && ~betacoef0asobs && ~isempty(p.Results.betacoef0)
+    warning('betacoef0 isnt doing anything for this linear least squares solution, use betacoef0asobs to include it');
+end
+end
