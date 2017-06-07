1d764928f808a3602752b8ee23542f2fbbcf5df5
Richie Slocum <slocumr@oregonstate.edu>
2017-06-07 01:00:14 -0700

    move generic code to general-purpose-matlab

--- chapters/leastsquares/alg/sandbox/ransac/calcRansacN.m
+++
@@ -1,6 +0,0 @@
-function N = calcRansacN(ransacconfidence,outlierprob,nPtsInSample)
-% calculate the number of iterations required to have no outliers with an
-% input confidence
-N = log(1-ransacconfidence)./log(1-(1-outlierprob).^nPtsInSample);
-
-end
